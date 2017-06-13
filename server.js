7749116ac81b59cf328936985f6339105632930f
sumit3005 <sumitcoeb30@gmail.com>
2017-06-13 07:35:37 +0000

    mm

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose=require('mongoose');
-var app = express();
-app.use(bodyParser.json());
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mittens');
+var app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+
+
+app.use(bodyParser.json());
+app.use(express.static('public')); 
+var Meow = mongoose.model('Meow', {
+ text: String
+});
+
+app.get('/meows', function(req, res, next){
+	Meow.find({}, function(err, meows){
+		return res.json(meows);
+	});
+});
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000')
+});
+
+
+app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+	});
+
+app.put('/meows/remove', function(req, res, next){	
+var meowId=req.body.meow._id;		
+		Meow.remove({_id: meowId}, function(err){
+			return res.send("Deleted Successfully");
+	});
 });
 
