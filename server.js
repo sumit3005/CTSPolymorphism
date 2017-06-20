c5f5fcd68db8af1f513e1471b45400e683192e97
sumit3005 <sumitcoeb30@gmail.com>
2017-06-20 07:37:37 +0000

    session 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/mittens');
 var app = express();
 
@@ -38,4 +39,42 @@ var meowId=req.body.meow._id;
 			return res.send("Deleted Successfully");
 	});
 });
+
+var User=mongoose.model('User', { 
+ username: String, password: String  });
+
+var bcrypt = require('bcryptjs');
+
+app.post('/users', function(req, res, next){	
+	bcrypt.genSalt(10, function(err,salt){
+		bcrypt.hash(req.body.password,salt,function(err,hash){
+			var newUser= new User({
+				username: req.body.username,
+				password: hash
+			});
+			newUser.save( function(err){
+			return res.send();
+	});
+		});
+	}) ;
+});
+
+var jwt = require('jwt-simple');
+var JWT_SECRET='catsmeow';
+
+app.put('/users/signin', function(req, res, next){
+
+	User.findOne({username: req.body.username},function(err,user){
+		bcrypt.compare(req.body.password, user.password, function(err, result){
+			if(result){
+				var token = jwt.encode(user, JWT_SECRET);
+				return res.json({token:token});
+			}
+			else{
+				return res.status(400).send();
+			}
+		});
+	});
+});
+
 
